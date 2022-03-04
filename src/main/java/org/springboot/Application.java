e17615836d54b05d61e9ce7988a35e91b354eac0
RavQs <ravenq1112@gmail.com>
2022-03-04 15:46:48 +0900

    Наконец-то доперло что нужно добавить сраный фильтр

--- src/main/java/org/springboot/Application.java
+++ src/main/java/org/springboot/Application.java
@@ -2,12 +2,19 @@ package org.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        return new HiddenHttpMethodFilter();
     }
 
 }
